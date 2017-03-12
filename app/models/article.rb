12202036baf2c581fff4702350e71e463d77b0e0
Nao Yukimori <sutoradaf2@gmail.com>
2017-03-12 22:03:49 +0900

    リファクタリング

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: destroy #関連付けて削除するときはこれをつけるのね
   validates :title, presence: true,
                       length: { minimum: 5 }
 end
